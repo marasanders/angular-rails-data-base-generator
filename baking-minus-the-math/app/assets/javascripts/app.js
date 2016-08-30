ffb6f2b3d5b20ab9240a34db48960cb048b060a1
Mara Sanders <sandersmara@gmail.com>
2016-08-29 23:26:34 -0400

    CRUD on all tables now functional

--- baking-minus-the-math/app/assets/javascripts/app.js
+++ baking-minus-the-math/app/assets/javascripts/app.js
@@ -243,27 +243,33 @@
     vm.new_recipe = new RecipeFactory(); //{category_id: $stateParams.id});
     console.log("ID"+$stateParams.id)
     console.log("NEW RECIPE "+JSON.stringify(vm.new_recipe))
+//Create new recipe
      vm.create = function(){
        console.log("in create function!")
       console.log("recipe title "+vm.new_recipe.title)
+//Recipe must have a title
         if (vm.new_recipe.title) {
+// Add new Recipe to database
            vm.new_recipe.$save({category_id: $stateParams.id}, function(response){
            console.log("IN SAVE NEW")
            console.log("response "+response)
+// New Recipe Id is not Accessible - update vm.recipes so it contains the new recipe
            RecipeFactory.query({category_id: $stateParams.id}).$promise.then(function(recipes){
              vm.recipes = recipes
              console.log("recipes after add "+JSON.stringify(recipes))
             //  var sortedData= array .sort((function (a, b) {
             //                   return new Date(b.date) - new Date(a.date)
             //                 }));
+// Take the Recipes and Sort them by Date so the Newest Recipe is Last - capture the new newRecipe in newRecipes sorted array
              var newRecipes = recipes.sort(function(a,b){return new Date(a.created_at) - new Date(b.created_at)});
              var newRecipe = {}
              newRecipe = newRecipes[recipes.length - 1]
              console.log("recipe id"+JSON.stringify(newRecipe))
              console.log("SORTED RECIPES"+JSON.stringify(newRecipes))
+// Now that we know the ID of the newly created recipe we can add ingredients which contain the foreign key recipe_id - redirect to edit where you can add update delete ingredients
              $state.go("recipeEdit", {id: newRecipe.id, category_id: $stateParams.id}, {reload: true})
            })
-          //  if(response.success) vm.recipes.push(response);
+//  if(response.success) vm.recipes.push(response) - update angular table to reflect new recipe on screen
            vm.new_recipe = new RecipeFactory(); //{category_id: $stateParams.id});
            console.log("ID"+$stateParams.id)
            console.log("NEW RECIPE "+vm.new_recipe)
@@ -275,51 +281,6 @@
          vm.new_recipe = {};
          vm.ingredients = {}
        }
-
-
-      //  vm.new_ingredient = new IngredientFactory(); //{category_id: $stateParams.id});
-      //  console.log("ID"+$stateParams.id)
-      //  console.log("IDr"+$stateParams.recipe_id)
-      //  console.log("NEW INGREDIENT "+JSON.stringify(vm.new_ingredient))
-      //   vm.create = function(){
-      //         vm.new_ingredient.$save({recipe_id: $stateParams.id}, function(response){
-      //           if(response.success) vm.new_ingredient.push(response);
-      //           vm.new_ingredient = new IngredientFactory();
-      //         })
-      //         vm.ingredients.push(angular.copy(vm.new_ingredient))
-      //         vm.new_ingredient = {}
-      //   }
-
-    //  vm.new_ingredient = new IngredientFactory(); //{category_id: $stateParams.id});
-    //  console.log("ID"+$stateParams.id)
-    //  console.log("IDr"+$stateParams.recipe_id)
-    //  console.log("NEW INGREDIENT "+JSON.stringify(vm.new_ingredient))
-    //   vm.new = function(){
-    //     IngredientFactory.query({recipe_id: vm.recipe.id}).$promise.then(function(ingredients){
-    //       vm.ingredients = ingredients
-    //       console.log("ingredients after recipe add "+JSON.stringify(ingredients))
-    //       if (!vm.ingredients){
-    //         vm.ingredients=[]
-          // }
-          // this.update = function(category){
-          //   category.$update(category);
-          // }
-        // })
-    //    console.log("ingredients after recipe add 222"+JSON.stringify(ingredients))
-    //     console.log("in create ingredient function!")
-    //    console.log("recipe componenet "+vm.new_ingredient)
-    //    vm.new_ingredient.$save({recipe_id: $stateParams.recipe_id}, function(response){
-    //      console.log("response "+response)
-    //      if(response.success) vm.new_ingredient.push(response);
-    //      vm.new_ingredient = new RecipeFactory(); //{category_id: $stateParams.id});
-    //      console.log("ID"+$stateParams.id)
-    //      console.log("NEW RECIPE "+vm.new_ingredient)
-    //  })
-    //     vm.ingredients.push(angular.copy(vm.new_ingredient));
-    //     vm.new_ingredient = {};
-    //   }
-
-
   }
   function RecipeFactoryFunction($resource) {
     // console.log()
@@ -425,6 +386,7 @@
      vm.create = function(){
      console.log("NEWINGREDIENT "+JSON.stringify(vm.new_ingredient.component))
         vm.new_ingredient.$save({recipe_id: $stateParams.id}, function(response){
+        vm.ingredients = IngredientFactory.query({recipe_id: $stateParams.id})
        console.log("RESPONSE "+JSON.stringify(response))
         if(response.success) vm.new_ingredient.push(response);
            vm.new_ingredient = new IngredientFactory();
@@ -460,9 +422,12 @@
 
       vm.ingredients = IngredientFactory.query({recipe_id: $stateParams.id})
       console.log("ingredients "+vm.ingredients)
+      var Ingredient = $resource("/ingredients/:id.json", {}, {
+          update: {method: "PUT"}
+        });
 
 
-      vm.update = function(recipe){
+      vm.updateRecipe = function(recipe){
         console.log(" update ID"+JSON.stringify($stateParams.id))
         console.log(" update catID"+JSON.stringify($stateParams.category_id))
 
@@ -496,6 +461,7 @@
          vm.create = function(){
          console.log("NEWINGREDIENT "+JSON.stringify(vm.new_ingredient.component))
             vm.new_ingredient.$save({recipe_id: $stateParams.id}, function(response){
+           vm.ingredients = IngredientFactory.query({recipe_id: $stateParams.id})
            console.log("RESPONSE "+JSON.stringify(response))
             if(response.success) vm.new_ingredient.push(response);
                vm.new_ingredient = new IngredientFactory();
@@ -510,10 +476,27 @@
         //   $state.go("categoryIndex", {}, {reload: true});
         // })
 
-      vm.destroy = function(index){
-        RecipeFactory.remove(index)
-        vm.recipe.splice(index, 1)
+      // vm.destroy = function(index){
+      //   RecipeFactory.remove(index)
+      //   vm.recipe.splice(index, 1)
+      // }
+
+        vm.updateIngredient = function(ing){
+          console.log("UPDATEINGREDIENT"+JSON.stringify(ing))
+          Ingredient.update({id: ing.id}, ing, function(response){
+            // ingredient.showEdit = !ingredient.showEdit
+          });
+          console.log("cat id"+$stateParams.category_id)
+          console.log("recipe id"+ing.recipe_id)
+        }
+
+      vm.destroy = function(ing){
+        console.log("index"+JSON.stringify(ing))
+        Ingredient.remove({id: ing.id}, function(response){
+          if(response.success) vm.ingredients.splice(vm.ingredients.indexOf(ing), 1);
+        })
       }
+
  }
 
 
